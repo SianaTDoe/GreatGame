c61e3e2bf5613cd38b553ca1906a35f9686b5f45
Petit Pain D'Epices <sianadoe@gmail.com>
2023-05-02 19:56:57 +0200

    move Red Panda + generate apples

--- script.js
+++ script.js
@@ -1,17 +1,31 @@
+let redPanda = document.querySelector(".redPanda");
+let currentPosition = 0;
 
+// Red Panda
+document.addEventListener("keydown", function (event) {
+  if (event.key === "ArrowRight") {
+    redPanda.style.transform = "rotateY(0)";
+    currentPosition += 50;
+    redPanda.style.left = currentPosition + "px";
+  } else {
+    redPanda.style.transform = "rotateY(180deg)"; 
+    currentPosition -= 50;
+    redPanda.style.left = currentPosition + "px";
+  }
+});
 
-let redPanda = document.querySelector('.redPanda');
+// Generate Apples
+let game = document.querySelector("#game");
 
-// Switch Red Panda
-document.addEventListener('keydown', function(event) {
-    if(event.key === "ArrowRight") {
-        redPanda.style.transform = 'rotateY(0)';
-    } else {
-        redPanda.style.transform = 'rotateY(180deg)';
-    }
-})
+function generateApple() {
+  let appleImage = document.createElement("img");
+  appleImage.src = "resources/apple.png";
+  appleImage.classList.add("apple");
+  appleImage.style.left =
+    Math.floor(Math.random() * (window.innerWidth - 50)) + "px";
+  game.appendChild(appleImage);
+}
 
-// Generate Apples 
-function apple() {
-
+for (let i = 0; i < 200; i++) {
+  generateApple();
 }
