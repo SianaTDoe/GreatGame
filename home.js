9218ad3216ee45d93aac7f090bf614b9ad90ea4e
Petit Pain D'Epices <sianadoe@gmail.com>
2023-05-03 14:21:20 +0200

    css home

--- home.js
+++ home.js
@@ -1,5 +1,5 @@
 import themes from "./themes.js"
-const title = document.querySelector('h1');
+const title = document.querySelector('.title');
 
 setInterval(() => {
    const color = (Math.random() * 0xffffff << 0).toString(16);
