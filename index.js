8cd968609588c622e47694c5723aa71154d6f969
Petit Pain D'Epices <sianadoe@gmail.com>
2023-05-04 13:12:24 +0200

    Co-authored-by: dany rose <Coeurtisol@users.noreply.github.com>

---
+++ index.js
@@ -0,0 +1,35 @@
+import themes from "./themes.js";
+
+const title = document.querySelector(".randomColor");
+function changeTitleColor() {
+  setInterval(() => {
+    const color = ((Math.random() * 0xffffff) << 0).toString(16);
+    title.style.color = `#${color}`;
+  }, 200);
+}
+changeTitleColor();
+
+const themeContainer = document.getElementById("themeContainer");
+function buildThemeList() {
+  const themeList = Object.entries(themes).map(
+    ([name, theme]) =>
+      `<span class="icon" data-theme="${name}">${theme.icon}</span>`
+  );
+  themeContainer.innerHTML = themeList.join("");
+}
+buildThemeList();
+
+const icons = document.querySelectorAll(".icon");
+const previewImg = document.querySelector("#preview>img");
+let selectedTheme;
+icons.forEach((i) => i.addEventListener("click", handleChangeTheme));
+function handleChangeTheme(e) {
+  selectedTheme = e.target.getAttribute("data-theme");
+  previewImg.src = `resources/${themes[selectedTheme].player}.png`;
+}
+
+const playBtn = document.getElementById("play");
+playBtn.addEventListener("click", handlePlay);
+function handlePlay() {
+  location.href = `game.html?theme=${selectedTheme}`;
+}
--- themes.js
+++ themes.js
@@ -1,17 +1,20 @@
 export default {
   redPanda: {
+    icon: "🍎",
     player: "redPanda",
     item: "apple",
     bomb: "panther",
     background: "backgroundBamboo",
   },
   dino: {
+    icon: "🥚",
     player: "dino",
     item: "egg",
     bomb: "meteor",
     background: "dinoBackground",
   },
   gigaChad: {
+    icon: "💪",
     player: "gigaChad",
     item: "whey",
     bomb: "syringe",
